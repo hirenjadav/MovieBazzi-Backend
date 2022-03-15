952ab665a2b0ba202a1315ab8a4f2e6e6ea2c4d0
Hiren Jadav <h.r.jadav252@gmail.com>
2022-03-15 11:37:17 +0530

    changes at port 2

--- index.js
+++ index.js
@@ -60,6 +60,11 @@ app.use("/api/reviews", reviews);
 
 // -----------------------------------------------------------------------------------
 
-app.listen(process.env.PORT || 8000, () => {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+app.listen(port, () => {
   console.log(`Server listening on port ${port}!`);
 });
