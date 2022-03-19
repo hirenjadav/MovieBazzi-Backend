9b2c74957e9643dbca4e9505938b01c7d53badc4
Hiren Jadav <h.r.jadav252@gmail.com>
2022-03-19 16:10:40 +0530

    changes

--- routes/users.js
+++ routes/users.js
@@ -72,7 +72,7 @@ router.put("/me/wishlist", auth, async (req, res) => {
 function validateWishlist(t) {
   const schema = Joi.object().keys({
     mediaType: Joi.string().required(),
-    mediaID: Joi.numbar().required(),
+    mediaID: Joi.number().required(),
   });
 
   return schema.validate(t);
